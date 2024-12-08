6f68c63989d83b8d72a0fa04648be28a193f7ba6
prokdo <prokdo@icloud.com>
2024-12-09 02:37:48 +0300

    MM lab2 fix (remake)

--- content/MM/lab2/src/main.py
+++ content/MM/lab2/src/main.py
@@ -1,55 +1,50 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-from substance_transfer import central_difference_scheme, cabare_scheme, right_corner_scheme, cross_scheme, solve, linear_combination_solve
+from substance_transfer import right_corner_scheme, central_difference_scheme, cabare_scheme, cross_scheme, linear_combination_scheme
 
 if __name__ == '__main__':
     # Параметры задачи
-    L: float = 150.0            # Длина области
-    T: float = 100.0            # Время моделирования
-    Nx: int = 100               # Количество узлов по пространству
-    Nt: int = 1000              # Количество шагов по времени
-    v: float = -0.5             # Скорость переноса
-    D: float = 0                # Коэффициент диффузии
-    dx: float = L / (Nx - 1)    # Шаг по пространству
-    dt: float = T / Nt          # Шаг по времени
+    L: float    = 160.0
+    T: int      = 100
+    v: float    = -0.5
+    dt: float   = 0.1
+    dx: float   = 1.0
+    Nx: int     = int(L / dx)
+    Nt: int     = int(T / dt)
 
-    # Исходная функция параболы
+    # Проверка условия Куранта
+    assert abs(v) * dt / dx <= 1, 'Условие Куранта не выполняется'
+
+    # Функция начального распределения концентрации
     def f(x: np.ndarray) -> np.ndarray:
-        c: np.ndarray = -0.01 * (x - 80)**2 + 1
-        c[c < 0] = 0
-        return c
-
-    # Проверка на устойчивость
-    assert v * dt / dx <= 1,            "Число Куранта слишком велико"
-    if D != 0: assert v * dx / D <= 1,  "Число Перкле слишком велико"
-    assert D * dt / dx**2 <= 0.5,       "Число Фурье слишком велико"
+        return np.maximum(-1 / 100 * (x - 80) ** 2 + 1, 0)
 
     # Инициализация сетки
-    x: np.ndarray = np.linspace(0, L, Nx)
+    x = np.arange(0, L, dx, dtype=float)
 
-    # Начальное условие: концентрация вещества сосредоточена в центре
-    c: np.ndarray = f(x)
-
-    # Получение решений для всех схем
-    c_exact: np.ndarray = f(x)
-    c_central: np.ndarray = solve(central_difference_scheme, Nt, c, v, D, dx, dt)
-    c_cabare: np.ndarray = solve(cabare_scheme, Nt, c, v, D, dx, dt)
-    c_right: np.ndarray = solve(right_corner_scheme, Nt, c, v, D, dx, dt)
-    # c_cross: np.ndarray = solve(cross_scheme, Nt, c, v, D, dx, dt)
-    c_cabare_central_linear_comb: np.ndarray = linear_combination_solve(cabare_scheme, central_difference_scheme, Nt, c, v, D, dx, dt)
+    # Вычисление значений для различных схем
+    c_init                      = f(x)
+    c_upwind                    = right_corner_scheme(Nt, c_init, v, dt, dx)
+    c_central                   = central_difference_scheme(Nt, c_init, v, dt, dx)
+    c_cabare                    = cabare_scheme(Nt, c_init, v, dt, dx)
+    # c_cross                     = cross_scheme(Nt, c_init, v, dt, dx)
+    c_upwind_cabare_combination = linear_combination_scheme(right_corner_scheme, cabare_scheme, Nt, c_init, v, dt, dx)
+    # c_cross_cabare_combination = linear_combination_scheme(cross_scheme, cabare_scheme, Nt, c_init, v, dt, dx)
 
     # Построение графиков
-    plt.figure(figsize=(12, 8), num="Лабораторная работа №2. Задача переноса веществ")
-    plt.plot(x, c_exact, label='Исходная функция параболы', color='black')
-    plt.plot(x, c_central, label="Центральная разностная схема", linestyle='-.')
-    plt.plot(x, c_cabare, label='Схема «кабаре»', linestyle='--')
-    plt.plot(x, c_right, label='Схема «правого уголка»', linestyle=(0, (1, 1)))
-    # plt.plot(x, c_cross, label='Схема «крест»', linestyle='-')
-    plt.plot(x, c_cabare_central_linear_comb, label='Линейная комбинация «кабаре» и центральной разностной схем', linewidth=2, linestyle=(0, (1, 1)))
-    plt.xlabel("x")
-    plt.ylabel("c(x)")
-    plt.title("Численные решения задачи переноса веществ")
-    plt.grid()
+    plt.figure(figsize=(12, 8), num='Лабораторная работа №2. Задача переноса веществ')
+    plt.plot(x, c_init,                         label='Исходная функция параболы',      color='black')
+    plt.plot(x, c_upwind,                       label='Схема «правый уголок»',          linestyle='--')
+    plt.plot(x, c_central,                      label='Центральная разностная схема',   linestyle='-.')
+    plt.plot(x, c_cabare,                       label='Схема «кабаре»',                 linestyle=':')
+    # plt.plot(x, c_cross,                        label='Схема «крест»',                  linestyle='--')
+    plt.plot(x, c_upwind_cabare_combination,    label='Линейная комбинация схем «правого уголка» и «кабаре»')
+    # plt.plot(x, c_cross_cabare_combination,    label='Линейная комбинация схем «правого уголка» и «кабаре»', linestyle='-.')
+
+    plt.title('Численные решения задачи переноса веществ')
+    plt.xlabel('x')
+    plt.ylabel('c(x)')
     plt.legend()
+    plt.grid()
     plt.show()
--- content/MM/lab2/src/substance_transfer.py
+++ content/MM/lab2/src/substance_transfer.py
@@ -2,122 +2,91 @@ from typing import Callable
 
 import numpy as np
 
-# Схема "против потока"
-def upwind_scheme(
-                    c: np.ndarray,
+# Схема "правый уголок"
+def right_corner_scheme(
+                    Nt: int,
+                    c_init: np.ndarray,
                     v: float,
-                    D: float,
-                    dx: float,
-                    dt: float) -> np.ndarray:
-    c_new = c.copy()
-    for i in range(1, len(c) - 1):
-        adv = -v * (c[i] - c[i - 1]) / dx
-        diff = D * (c[i + 1] - 2 * c[i] + c[i - 1]) / dx**2
-        c_new[i] = c[i] + dt * (adv + diff)
-    return c_new
+                    dt: float,
+                    dx: float) -> np.ndarray:
+    c = np.zeros((Nt, c_init.size))
+    c[0] = c_init
+    for n in range(1, Nt):
+        for i in range(c_init.size):
+            c_n_i = c[n - 1, i]
+            if v > 0:
+                c_n_im1 = c[n - 1, i - 1] if i - 1 >= 0 else 0
+                c[n, i] = c_n_i - v * dt * (c_n_i - c_n_im1) / dx
+            else:
+                c_n_ip1 = c[n - 1, i + 1] if i + 1 < c_init.size else 0
+                c[n, i] = c_n_i - (-v) * dt * (c_n_i - c_n_ip1) / dx
+    return c[-1]
 
 # Центральная разностная схема
 def central_difference_scheme(
-                    c: np.ndarray,
+                    Nt: int,
+                    c_init: np.ndarray,
                     v: float,
-                    D: float,
-                    dx: float,
-                    dt: float) -> np.ndarray:
-    c_new = c.copy()
-    for i in range(1, len(c) - 1):
-        adv = -v * (c[i + 1] - c[i - 1]) / (2 * dx)
-        diff = D * (c[i + 1] - 2 * c[i] + c[i - 1]) / dx**2
-        c_new[i] = c[i] + dt * (adv + diff)
-    return c_new
+                    dt: float,
+                    dx: float) -> np.ndarray:
+    c = np.zeros((Nt, c_init.size))
+    c[0] = c_init
+    for n in range(1, Nt):
+        for i in range(c_init.size):
+            c_n_i = c[n - 1, i]
+            c_n_ip1 = c[n - 1, i + 1] if i + 1 < c_init.size else 0
+            c_n_im1 = c[n - 1, i - 1] if i - 1 >= 0 else 0
+            c[n, i] = c_n_i - v * dt * (c_n_ip1 - c_n_im1) / (2 * dx)
+    return c[-1]
 
 # Схема "кабаре"
 def cabare_scheme(
-                    c: np.ndarray,
+                    Nt: int,
+                    c_init: np.ndarray,
                     v: float,
-                    D: float,
-                    dx: float,
-                    dt: float) -> np.ndarray:
-    c_new = c.copy()
-    for i in range(1, len(c) - 1):
-        if v < 0:
-            adv_flux = -v * (c[i + 1] - c[i]) / dx
-        else:
-            adv_flux = -v * (c[i] - c[i - 1]) / dx
-        diff_flux = D * (c[i + 1] - 2 * c[i] + c[i - 1]) / dx**2
-        c_new[i] = c[i] + dt * (adv_flux + diff_flux)
-    return c_new
+                    dt: float,
+                    dx: float) -> np.ndarray:
+    c = np.zeros((Nt, c_init.size))
+    c[0] = c_init
+    for n in range(1, Nt):
+        for i in range(c_init.size):
+            c_n_i = c[n - 1, i]
+            if v > 0:
+                c_n_im1 = c[n - 1, i - 1] if i - 1 >= 0 else 0
+                c_nm1_im1 = c[n - 2, i - 1] if (i - 1 >= 0 and n - 2 >= 0) else 0
+                c[n, i] = c_n_i - (c_n_im1 - c_nm1_im1) - (2 * dt * v) / dx * (c_n_i - c_n_im1)
+            else:
+                c_n_ip1 = c[n - 1, i + 1] if i + 1 < c_init.size else 0
+                c_nm1_ip1 = c[n - 2, i + 1] if (i + 1 < c_init.size and n - 2 >= 0) else 0
+                c[n, i] = c_n_i - (c_n_ip1 - c_nm1_ip1) - (2 * dt * -v) / dx * (c_n_i - c_n_ip1)
+    return c[-1]
 
 # Схема "крест"
 def cross_scheme(
-                    c: np.ndarray,
+                    Nt: int,
+                    c_init: np.ndarray,
                     v: float,
-                    D: float,
-                    dx: float,
-                    dt: float) -> np.ndarray:
-    c_new = c.copy()
-    for i in range(1, len(c) - 1):
-        adv = -v * (c[i + 1] - c[i - 1]) / (2 * dx)
-        diff = D * (c[i + 1] - 2 * c[i] + c[i - 1]) / dx**2
-        c_new[i] = c[i] + dt * (adv + diff)
-    return c_new
+                    dt: float,
+                    dx: float) -> np.ndarray:
+    c = np.zeros((Nt, c_init.size))
+    c[0] = c_init
+    c[1] = c_init
 
-# Схема "правый уголок"
-def right_corner_scheme(
-                    c: np.ndarray,
-                    v: float,
-                    D: float,
-                    dx: float,
-                    dt: float) -> np.ndarray:
-    c_new = c.copy()
-    for i in range(1, len(c) - 1):
-        adv = -v * (c[i + 1] - c[i]) / dx
-        diff = D * (c[i + 1] - 2 * c[i] + c[i - 1]) / dx**2
-        c_new[i] = c[i] + dt * (adv + diff)
-    return c_new
+    for n in range(1, Nt - 1):
+        for i in range(1, c_init.size - 1):
+            c[n + 1, i] = c[n - 1, i] - v * dt * (c[n, i + 1] - c[n, i - 1]) / (2 * dx)
+    return c[-1]
 
-# Линейная комбинация двух схем
+# Линейная комбинация двух численных схем
 def linear_combination_scheme(
-                    first_scheme: Callable[[np.ndarray, float, float, float, float], np.ndarray],
-                    second_scheme: Callable[[np.ndarray, float, float, float, float], np.ndarray],
-                    c: np.ndarray,
-                    v: float,
-                    D: float,
-                    dx: float,
-                    dt: float,
-                    alpha: float = 0.5) -> np.ndarray:
-    c_first = first_scheme(c, v, D, dx, dt)
-    c_second = second_scheme(c, v, D, dx, dt)
-    c_new = alpha * c_first + (1 - alpha) * c_second
-    return c_new
-
-def solve(
-                    scheme: Callable[[np.ndarray, float, float, float, float], np.ndarray],
+                    first_scheme: Callable[[int, np.ndarray, float, float, float], np.ndarray],
+                    second_scheme: Callable[[int, np.ndarray, float, float, float], np.ndarray],
                     Nt: int,
-                    c: np.ndarray,
+                    c_init: np.ndarray,
                     v: float,
-                    D: float,
-                    dx: float,
-                    dt: float) -> np.ndarray:
-    c_new: np.ndarray = c.copy()
-    for _ in range(Nt):
-        c_new = scheme(c_new, v, D, dx, dt)
-    total_mass = np.sum(c)
-    c_new *= total_mass / np.sum(c_new)
-    return c_new
-
-def linear_combination_solve(
-                    first_scheme: Callable[[np.ndarray, float, float, float, float], np.ndarray],
-                    second_scheme: Callable[[np.ndarray, float, float, float, float], np.ndarray],
-                    Nt: int,
-                    c: np.ndarray,
-                    v: float,
-                    D: float,
-                    dx: float,
                     dt: float,
+                    dx: float,
                     alpha: float = 0.5) -> np.ndarray:
-    c_new: np.ndarray = c.copy()
-    for _ in range(Nt):
-        c_new = linear_combination_scheme(first_scheme, second_scheme, c_new, v, D, dx, dt, alpha)
-    total_mass = np.sum(c)
-    c_new *= total_mass / np.sum(c_new)
-    return c_new
+    c_first = first_scheme(Nt, c_init, v, dt, dx)
+    c_second = second_scheme(Nt, c_init, v, dt, dx)
+    return alpha * c_first + (1 - alpha) * c_second
